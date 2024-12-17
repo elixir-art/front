4d126345d4b5efdb662dfe54cb336e9fd3aeda86
Oleksandr Chernyonkov <oleksandr.chernyonkov@gmail.com>
2024-12-18 00:03:30 +0200

    removed redundant lines

--- my_calendar/assets/js/app.js
+++ my_calendar/assets/js/app.js
@@ -47,16 +47,13 @@ window.liveSocket = liveSocket
 document.addEventListener("DOMContentLoaded", () => {
   const themeToggle = document.getElementById("theme-toggle");
 
-  // При загрузке проверяем, если сохранена темная тема в localStorage
   if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
   }
 
-  // Переключение темы
   themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
-    // Сохраняем выбор в localStorage
     if (document.body.classList.contains("dark")) {
       localStorage.setItem("theme", "dark");
     } else {
