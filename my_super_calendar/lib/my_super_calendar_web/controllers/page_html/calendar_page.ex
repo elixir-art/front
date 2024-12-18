e95c4c086791175b2e17bf4c17bd6747dd2acc07
potrap <maksym.potrap@introduct.tech>
2024-12-18 10:33:18 +0200

    fixed html

--- my_super_calendar/lib/my_super_calendar_web/controllers/page_html/calendar_page.ex
+++ my_super_calendar/lib/my_super_calendar_web/controllers/page_html/calendar_page.ex
@@ -20,7 +20,7 @@ defmodule MySuperCalendarWeb.CalendarPage do
       </button>
     </div>
 
-    <div class="w-[563px] h-[560px] rounded-xl fixed top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] p-[72px] bg-white dark:bg-gray-900">
+    <div class="w-[560px] h-[560px] rounded-xl fixed top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] p-[72px] bg-white dark:bg-gray-900">
       <div class="flex justify-between items-center mb-[52px]">
         <p class="font-bold text-black dark:text-white">December 2024</p>
         <div class="flex gap-[5px]">
@@ -38,16 +38,16 @@ defmodule MySuperCalendarWeb.CalendarPage do
       <ul class="flex flex-wrap gap-[3px]">
         <%= for {month_day, index} <- Enum.with_index(@days_in_month) do %>
           <%= if index == 0 do %>
-            <li class="flex justify-center items-center ml-[180px] w-[55px] h-[55px] text-gray-500 dark:text-gray-400">
+            <li class="flex justify-center items-center ml-[180px] w-[56px] h-[56px] text-gray-500 dark:text-gray-400">
               <%= month_day %>
             </li>
           <% else %>
             <%= if month_day == 18 do %>
-              <li class="flex justify-center items-center w-[55px] h-[55px] rounded-full bg-[#e83b27] dark:bg-lime-600 text-white">
+              <li class="flex justify-center items-center w-[56px] h-[56px] rounded-full bg-[#e83b27] dark:bg-lime-600 text-white">
                 <%= month_day %>
               </li>
             <% else %>
-              <li class="flex justify-center items-center w-[55px] h-[55px] text-gray-500 dark:text-gray-400">
+              <li class="flex justify-center items-center w-[56px] h-[56px] text-gray-500 dark:text-gray-400">
                 <%= month_day %>
               </li>
             <% end %>
