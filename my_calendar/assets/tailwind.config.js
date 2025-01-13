5543a763417599b3bf959c6056408fe15c559e52
Oleg <franky.ua@gmail.com>
2025-01-13 11:03:19 +0200

    feat: added left panel

--- my_calendar/assets/tailwind.config.js
+++ my_calendar/assets/tailwind.config.js
@@ -19,7 +19,6 @@ module.exports = {
         heading: ["Open Sans", "sans-serif"], 
       },
       fontSize: {
-
         '100': '18px',
         'xl': '32px',
         '2xl': '42px', 
@@ -30,16 +29,18 @@ module.exports = {
           blue: "#E8F0FB",
           peach: "#FFEFE7",
           gray: "#B2B2B2",
+          gray2: "#686868",
+          gray3: "#F1F1F1",
           violet1: "#1B204A",
           violet2: "#161E54"
         },
+        active: "#FF5151",
         grayInput: '#B3B3B3',
         accent_text: "#D97706",
         accent_blue: "#3786F1",
         accent_rose: "#FF5151",
         accent_pink: "#EE61CF",
         mainColor: '#FF5151',
-        orange: "#FF5151",
         coldBlack: "black",
         customGray: "gray"
       },
--- my_calendar/lib/my_calendar_web/live/dashboard.ex
+++ my_calendar/lib/my_calendar_web/live/dashboard.ex
@@ -1,74 +1,117 @@
 defmodule MyCalendarWeb.Dashboard do
   use MyCalendarWeb, :live_view
 
-  alias MyCalendarWeb.Dashboard
   alias MyCalendarWeb.IconComponent
 
   def render(assigns) do
     ~H"""
-      <div class="container mx-auto">
-
-        <div class="flex justify-between items-center">
-          <div class="flex justify-between gap-x-5 items-center">
-          <IconComponent.render
-            id="burger-menu"
-            name="burger"
-
-          />
-          <div class="relative">
-            <input type="text" placeholder="Search" class="hidden md:inline-block md:w-60 lg:w-80 bg-gray-50 border-gray-200 rounded-lg text-sm text-grayInput">
-             <IconComponent.render
-            id="search"
-            name="search"
-            class="absolute top-1/2 -translate-y-1/2 left-0 md:left-48 lg:left-72"
-          />
-         </div>
-
+    <div class="flex">
+      <div class="bg-gray-50 hidden md:block">
+        <p class="px-4 pt-10 pb-5 font-semibold text-center text-xl lg:text-4xl lg:px-16 lg:pt-8">
+          WeHR
+        </p>
+        <div class="pt-3 flex flex-col items-center lg:items-start lg:pl-8">
+          <p class="text-center lg:text-start mb-5 font-roboto font-medium opacity-50 text-gray2 text-xxs">
+            MAIN MENU
+          </p>
+          <div class="flex items-center">
+            <IconComponent.render name="dashboard" fill="#FF5151" />
+            <p class="hidden lg:block font-medium text-active ml-6">Dashboard</p>
           </div>
-          <div class="flex justify-between gap-x-4 items-center md:gap-x-8">
-          <IconComponent.render
-            id="notification"
-            name="notification"
-            fill="#B2B2B2"
-            stroke="#B2B2B2"
-          />
-          <IconComponent.render
-            id="quote"
-            name="quote"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            strokeWidth="0"
-          />
-          <img src={~p"/images/profile.png"} alt="photo" width="36" height="36" />
-          <IconComponent.render
-            id="arrow"
-            name="arrow"
-            fill="white"
-            stroke="#B2B2B2"
-          />
+          <div class="flex items-center my-9">
+            <IconComponent.render name="recruitment" />
+            <p class="hidden lg:block ml-6 text-primarySubtitle">Recruitment</p>
+          </div>
+          <div class="flex items-center">
+            <IconComponent.render name="calendar" />
+            <p class="hidden lg:block ml-6 text-primarySubtitle">Schedule</p>
+          </div>
+          <div class="flex items-center my-9">
+            <IconComponent.render name="employee" />
+            <p class="hidden lg:block ml-6 text-primarySubtitle">Employee</p>
+          </div>
+          <div class="flex items-center">
+            <IconComponent.render name="department" />
+            <p class="hidden lg:block ml-6 text-primarySubtitle">Department</p>
+          </div>
+          <p class="text-center mt-10 font-roboto font-medium opacity-50 text-gray2 text-xxs">
+            OTHER
+          </p>
+          <div class="flex items-center mt-5 mb-9">
+            <IconComponent.render name="support" />
+            <p class="hidden lg:block ml-6 text-primarySubtitle">Support</p>
+          </div>
+          <div class="flex items-center">
+            <IconComponent.render name="settings" />
+            <p class="hidden lg:block ml-6 text-primarySubtitle">Settings</p>
           </div>
         </div>
       </div>
 
-      <div class="container mx-auto bg-white">
-        <h1 class="pt-5 text-xl font-medium py-5 xl:block">Dashboard</h1>
-        <div class="lg:flex lg:justify-between lg:gap-6">
-          <div class="">
-            <section class="py-2">
-              <div class="flex flex-col gap-5 md:justify-center">
-                <div class="flex flex-col gap-5 md:flex-row md:justify-center">
-                  <%= for card <- @first_cards do %>
-                    <div class={"rounded-xl p-6 flex flex-col gap-5 #{card.background_color} md:w-1/3" }>
-                      <h2 class="font-medium text-lg"><%= card.title %></h2>
-                      <p class="text-xl font-medium	"><%= card.number %></p>
-                      <p class={"text-lg whitespace-nowrap #{card.text_color}"}><%= card.description %></p>
-                    </div>
-                  <% end %>
-                </div>
-                <div class="flex flex-col gap-5 md:flex-row md:justify-center">
-                  <%= for card <- @second_cards do %>
-                    <div class="rounded-xl p-6 flex items-center justify-between border-2 border-gray-100 md:w-1/2 md:justify-center">
+      <div>
+        <div class="container mx-auto py-2 px-5 md:px-14 lg:py-5">
+          <div class="flex justify-between items-center">
+            <div class="flex justify-between gap-x-5 items-center">
+              <IconComponent.render id="burger-menu" name="burger" class="lg:hidden" />
+              <div class="relative">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  class="hidden md:inline-block md:w-60 lg:w-80 bg-gray-50 border-gray-200 rounded-lg text-sm text-grayInput"
+                />
+                <IconComponent.render
+                  id="search"
+                  name="search"
+                  class="absolute top-1/2 -translate-y-1/2 left-0 md:left-48 lg:left-72"
+                />
+              </div>
+            </div>
+            <div class="flex justify-between gap-x-4 items-center md:gap-x-8">
+              <IconComponent.render
+                id="notification"
+                name="notification"
+                fill="#B2B2B2"
+                stroke="#B2B2B2"
+              />
+              <IconComponent.render
+                id="quote"
+                name="quote"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                strokeWidth="0"
+              />
+              <div class="flex gap-1.5 items-center">
+                <img src={~p"/images/profile.png"} alt="photo" width="36" height="36" />
+                <span class="hidden lg:block ml-2 text-100 medium">Admira John</span>
+                <IconComponent.render id="arrow" name="arrow" fill="white" stroke="#B2B2B2" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class='w-full h-px bg-card_bg-gray3'></div>
+
+        <div class="container mx-auto bg-white px-5 pb-6 md:px-14">
+          <h1 class="pt-5 text-xl font-medium py-5 xl:block">Dashboard</h1>
+          <div class="lg:flex lg:justify-between lg:gap-6">
+            <div class="">
+              <section class="py-2">
+                <div class="flex flex-col gap-5 md:justify-center">
+                  <div class="flex flex-col gap-5 md:flex-row md:justify-center md:items-stretch">
+                    <%= for card <- @first_cards do %>
+                      <div class={"rounded-xl p-6 flex flex-col flex-1 gap-5 #{card.background_color} md:w-1/3" }>
+                        <h2 class="font-medium text-lg"><%= card.title %></h2>
+                        <p class="text-xl font-medium	"><%= card.number %></p>
+                        <p class={"text-lg whitespace-nowrap #{card.text_color}"}>
+                          <%= card.description %>
+                        </p>
+                      </div>
+                    <% end %>
+                  </div>
+                  <div class="flex flex-col gap-5 md:flex-row md:justify-center">
+                    <%= for card <- @second_cards do %>
+                      <div class="rounded-xl p-6 flex items-center justify-between border-2 border-gray-100 md:w-1/2 md:justify-center">
                         <div class="">
                           <h2 class="font-medium text-lg mb-5"><%= card.title %></h2>
                           <p class="text-2xl font-medium mb-6"><%= card.number %></p>
@@ -77,78 +120,77 @@ defmodule MyCalendarWeb.Dashboard do
                             <p class=""><%= "#{card.women} women" %></p>
                           </div>
                         </div>
-                      <div>
-                        <img
-                          class="mb-5"
-                          src={~p"/images/#{card.image}"}
-                          alt="graph"
-                          width="115"
-                          height="78"
-                        />
-                        <p class="text-sm text-right bg-card_bg-peach p-1 rounded">
-                          <%= "+#{card.persantage}% Past month" %>
-                        </p>
-                      </div>
-                    </div>
-                  <% end %>
-                </div>
-              </div>
-             </section>
-
-            <div>
-              <div class="rounded-t-xl border-2 border-gray-100 rounded-lg mt-4">
-                <div class="flex justify-between items-center pt-4 px-5">
-                  <h2 class="font-medium text-lg">Announcement</h2>
-                  <button
-                    class="flex items-center gap-1 border-2 border-gray-100 p-3 rounded-md text-sm text-gray-400"
-                    type="button"
-                  >
-                    Today, 13 Sep 2021
-                  </button>
-                </div>
-                <ul class="flex flex-col gap-3 px-5 pt-4 pb-2">
-                  <%= for card <- @third_cards do %>
-                    <li class="rounded-xl p-6 flex flex-col gap-3 border-2 border-gray-100 bg-gray-50">
-                      <h3 class="text-lg"><%= card.title %></h3>
-                      <div class="flex justify-between items-center">
-                        <p class="text-gray-600 text-sm"><%= card.date_time %></p>
-                        <div class="flex gap-3">
-                          <IconComponent.render
-                            id="staple"
-                            name="staple"
-                            fill={card.fill}
+                        <div>
+                          <img
+                            class="mb-5"
+                            src={~p"/images/#{card.image}"}
+                            alt="graph"
+                            width="115"
+                            height="78"
                           />
-                          <IconComponent.render
-                            id="overflow"
-                            name="overflow"
-                          />
+                          <p class="text-sm text-right bg-card_bg-peach p-1 rounded">
+                            <%= "+#{card.persantage}% Past month" %>
+                          </p>
                         </div>
                       </div>
-                    </li>
-                  <% end %>
-                </ul>
-              <button
-                class="w-full text-center p-3 border-t-2 border-gray-100 font-medium text-lg text-orange"
-                type="button"
-              >
-                See All Announcement
-              </button>
+                    <% end %>
+                  </div>
+                </div>
+              </section>
+
+              <div>
+                <div class="rounded-t-xl border-2 border-gray-100 rounded-lg mt-4">
+                  <div class="flex justify-between items-center pt-4 px-5">
+                    <h2 class="font-medium text-lg">Announcement</h2>
+                    <button
+                      class="flex items-center gap-1 border-2 border-gray-100 p-3 rounded-md text-sm text-gray-400"
+                      type="button"
+                    >
+                      Today, 13 Sep 2021
+                    </button>
+                  </div>
+                  <ul class="flex flex-col gap-3 px-5 pt-4 pb-2">
+                    <%= for card <- @third_cards do %>
+                      <li class="rounded-xl p-6 flex flex-col gap-3 border-2 border-gray-100 bg-gray-50">
+                        <h3 class="text-lg"><%= card.title %></h3>
+                        <div class="flex justify-between items-center">
+                          <p class="text-gray-600 text-sm"><%= card.date_time %></p>
+                          <div class="flex gap-3">
+                            <IconComponent.render id="staple" name="staple" fill={card.fill} />
+                            <IconComponent.render id="overflow" name="overflow" />
+                          </div>
+                        </div>
+                      </li>
+                    <% end %>
+                  </ul>
+                  <button
+                    class="w-full text-center p-3 border-t-2 border-gray-100 font-medium text-lg text-mainColor"
+                    type="button"
+                  >
+                    See All Announcement
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
             <section class="grid grid-cols-1 md:grid-cols-[1.5fr_2fr] md:gap-4 lg:flex lg:flex-col">
               <div class="rounded-lg overflow-hidden mt-5">
                 <div class="bg-card_bg-violet1">
-                  <h3 class="text-white text-100 font-poppins font-medium py-4 ml-5 mr-2.5">Recent Activity</h3>
+                  <h3 class="text-white text-100 font-poppins font-medium py-4 ml-5 mr-2.5">
+                    Recent Activity
+                  </h3>
                 </div>
                 <div class="flex-cols justify-items-start bg-card_bg-violet2 text-white pl-5 pb-8 pr-10 rounded-b-lg">
                   <p class="font-roboto text-xs opacity-60 pt-3">10.40 AM, Fri 10 Sept 2021</p>
                   <p class="text-100 font-medium py-2">You Posted a New Job</p>
-                  <p class="text-sm opacity-80 mb-7">Kindly check the requirements and terms of work and make sure everything is
-                    right.</p>
+                  <p class="text-sm opacity-80 mb-7">
+                    Kindly check the requirements and terms of work and make sure everything is
+                    right.
+                  </p>
                   <p class="text-base font-roboto mb-7">Today you made 12 Activities</p>
-                  <button class="w-36 bg-orange rounded-md font-poppins font-medium capitalize text-sm py-2.5 px-4">See all activity</button>
+                  <button class="w-36 bg-orange rounded-md font-poppins font-medium capitalize text-sm py-2.5 px-4">
+                    See all activity
+                  </button>
                 </div>
               </div>
 
@@ -168,10 +210,7 @@ defmodule MyCalendarWeb.Dashboard do
                     <h3 class="text-lg px-2 pt-1">Review candidate applications</h3>
                     <div class="flex justify-between items-center px-2 pt-1">
                       <p class="text-sm text-gray-500">5 minutes ago</p>
-                      <IconComponent.render
-                            id="overflow"
-                            name="overflow"
-                          />
+                      <IconComponent.render id="overflow" name="overflow" />
                     </div>
                   </div>
                   <p class="pt-5 mb-2">Other</p>
@@ -181,17 +220,14 @@ defmodule MyCalendarWeb.Dashboard do
                         <h2 class="text-lg px-2 pt-1"><%= card.title %></h2>
                         <div class="flex justify-between items-center px-2 pt-1">
                           <p class="text-sm text-gray-500"><%= card.date_time %></p>
-                          <IconComponent.render
-                            id="overflow"
-                            name="overflow"
-                          />
+                          <IconComponent.render id="overflow" name="overflow" />
                         </div>
                       </li>
                     <% end %>
                   </ul>
                 </div>
                 <button
-                  class="w-full text-center font-medium text-lg text-orange p-3 border-t-2 border-gray-100"
+                  class="w-full text-center font-medium text-lg text-mainColor p-3 border-t-2 border-gray-100"
                   type="button"
                 >
                   See All Announcement
@@ -199,7 +235,9 @@ defmodule MyCalendarWeb.Dashboard do
               </div>
             </section>
           </div>
+        </div>
       </div>
+    </div>
     """
   end
 
