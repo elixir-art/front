67aef5bd38c3b6989de414fbe312278e9f86755f
Lestrn <ilestrn@gmail.com>
2025-01-20 15:03:06 +0200

    more progress on form

--- lib/calander_web/controllers/page_html/form.ex
+++ lib/calander_web/controllers/page_html/form.ex
@@ -22,44 +22,140 @@ defmodule CalanderWeb.PageHtml.Form do
             <% end %>
           </ul>
         </div>
-        <div class="mb-[40px]">
-          <h2 class="font-medium mb-[10px]">Contact details</h2>
-          <p class="text-xs text-gray-300">Lorem ipsum dolor sit amet consectetur adipisc.</p>
-        </div>
-        <div class="flex flex-col gap-[20px]">
-          <div class="flex basis1/2 gap-[20px]">
-            <div class="flex flex-col">
-              <label class="mb-[10px]" for="name">Name</label>
-              <input type="text" id="name" name="name" class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2" placeholder="John Carter" />
+        <secton class="hidden">
+          <div class="mb-[40px]">
+            <h2 class="font-medium mb-[10px]">Contact details</h2>
+            <p class="text-xs text-gray-300">Lorem ipsum dolor sit amet consectetur adipisc.</p>
+          </div>
+          <div class="flex flex-col gap-[20px]">
+            <div class="flex basis1/2 gap-[20px]">
+              <div class="flex flex-col">
+                <label class="mb-[10px]" for="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2"
+                  placeholder="John Carter"
+                />
+              </div>
+              <div class="flex flex-col">
+                <label class="mb-[10px]" for="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2"
+                  placeholder="Email adress"
+                />
+              </div>
             </div>
-            <div class="flex flex-col">
-              <label class="mb-[10px]" for="email">Email</label>
-              <input type="email" id="email" name="email" class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2" placeholder="Email adress" />
+            <div class="flex basis-1/2 gap-[20px]">
+              <div class="flex flex-col">
+                <label class="mb-[10px]" for="phone">Phone Number</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2"
+                  placeholder="(123) 456 - 7890"
+                />
+              </div>
+              <div class="flex flex-col">
+                <label class="mb-[10px]" for="company">Company</label>
+                <input
+                  type="text"
+                  id="company"
+                  name="company"
+                  class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2"
+                  placeholder="Company name"
+                />
+              </div>
             </div>
           </div>
-          <div class="flex basis-1/2 gap-[20px]">
-            <div class="flex flex-col">
-              <label class="mb-[10px]" for="phone">Phone Number</label>
-              <input type="tel" id="phone" name="phone" class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2" placeholder="(123) 456 - 7890" />
-            </div>
-            <div class="flex flex-col">
-              <label class="mb-[10px]" for="company">Company</label>
-              <input type="text" id="company" name="company" class="form-input rounded-full p-[15px] shadow-md border-gray-300 border-2" placeholder="Company name" />
+        </secton>
+        <section class="hidden">
+          <div class="mb-[40px]">
+            <h2 class="font-medium mb-[10px]">Our services</h2>
+            <p class="text-xs text-gray-300">Please select which service you are interested in.</p>
+          </div>
+          <div class="flex flex-wrap gap-[15px]">
+            <%= for second_section_blocks <- @second_section_blocks do %>
+              <div class="p-[20px] bg-green-300 w-[48%]"><%= second_section_blocks.text %></div>
+            <% end %>
+          </div>
+        </section>
+        <section class="hidden">
+          <div class="mb-[40px]">
+            <h2 class="font-medium mb-[10px]">What’s your project budget?</h2>
+            <p class="text-xs text-gray-300">
+              Please select the project budget range you have in mind.
+            </p>
+          </div>
+          <div class="flex flex-wrap gap-[10px]">
+            <%= for third_section_block <- @third_section_blocks do %>
+              <div class="bg-green-300 w-[48%] py-[30px] px-[10px]">
+                <div class="ml-[30px]">
+                  <input
+                    type="radio"
+                    id={"#{third_section_block}"}
+                    name="project-budjet"
+                    value={"#{third_section_block}"}
+                    }
+                  />
+                  <label for={"#{third_section_block}"}><%= third_section_block %></label>
+                </div>
+              </div>
+            <% end %>
+          </div>
+        </section>
+        <section class="">
+          <div class="flex justify-center min-h-[268px]">
+            <div class="flex flex-wrap flex-col items-center justify-center w-[70%]">
+              <h2 class="font-medium mb-[10px]">Submit your quote request</h2>
+              <div class="mb-[15px]">
+                <p class="text-xs text-gray-300 text-center">
+                  Please review all the information you previously typed in the past steps, and if all is okay, submit your message to receive a project quote in 24 - 48 hours.
+                </p>
+              </div>
+              <button type="submit" id="submit-btn">Submit</button>
             </div>
           </div>
-        </div>
-        <button type="submit" id="submit-btn">Submit</button>
+        </section>
       </div>
     </form>
     """
   end
 
   def mount(_params, _session, socket) do
+    second_section_blocks = [
+      %{
+        text: "Development",
+        icon: ""
+      },
+      %{
+        text: "Web Design",
+        icon: ""
+      },
+      %{
+        text: "Marketing",
+        icon: ""
+      },
+      %{
+        text: "Other",
+        icon: ""
+      }
+    ]
+
     {:ok,
      socket
      |> assign(show_animation: false)
      |> assign(current_step: 0)
-     |> assign(sections: [1, 2, 3, 4])}
+     |> assign(sections: [1, 2, 3, 4])
+     |> assign(second_section_blocks: second_section_blocks)
+     |> assign(
+       third_section_blocks: ["$5.000-$10.000", "$10.000-$20.000", "$20.000-$50.000", "$50.000 +"]
+     )}
   end
 
   def handle_event("test", _, socket) do
