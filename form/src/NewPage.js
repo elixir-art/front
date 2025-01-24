9ed5eab14aa8815678a7b9b6957b30d826cfc374
Vlada <proskurina.vlada@gmail.com>
2025-01-24 18:00:17 +0200

    add form tabs

--- form/src/NewPage.js
+++ form/src/NewPage.js
@@ -1,7 +1,16 @@
-import React, { useState } from"react";
+import React, { useState } from "react";
 
 const NewPage = () => {
+  // const [selectedServices, setSelectedServices] = useState([]);
+
+  const services = [
+    { value: "development", label: "Development", icon: "\u{1F4BB}" },
+    { value: "web_design", label: "Web Design", icon: "\u{1F4BB}" },
+    { value: "marketing", label: "Marketing", icon: "\u{1F4E3}" },
+    { value: "other", label: "Other", icon: "\u{2699}" },
+  ];
   const [step, setStep] = useState(1);
+  const [submit, setSubmit] = useState(false);
 
   const handleNextStep = () => {
     setStep((prevStep) => prevStep + 1);
@@ -9,6 +18,18 @@ const NewPage = () => {
   const handlePreviousStep = () => {
     setStep((prevStep) => prevStep - 1);
   };
+  const handleSubmit = () => {
+    setSubmit((prevSubmit) => !prevSubmit);
+  };
+
+  // const setValues = (e) => {
+  //   console.log(e.target);
+  //   // setSelectedServices((prevState) => prevState - 1);
+  // };
+
+  const formButton =
+    "p-11 rounded-xl shadow-md border border-neutral_300 peer-checked:border-primary_blue peer-checked:border-2";
+  const h3 = "flex item-start text-xl font-bold mb-2";
 
   return (
     <div>
@@ -24,41 +45,41 @@ const NewPage = () => {
           </p>
           <div className="border border-neutral_300 rounded-2xl shadow-md  px-11 pb-20">
             <div className="flex justify-center pt-8">
-              <div className="divide-y">
-                <div>01</div>
-                <div>02</div>
-              </div>
-              {[1, 2, 3, 4].map((s) => (
-                <div key={s} className="flex items-center">
-                  <div
-                    className={`w-9 h-9 mx-4 mb-8 flex items-center justify-center ${
-                      step >= s ?"bg-primary_blue" :"bg-neutral_300"
-                    } text-white rounded-full`}
-                  >
-                    {s}
-                  </div>
-                  {s !== 4 && s !== step && (
-                    <div
-                      className={`w-24 h-2 mb-8 rounded-full ${
-                        step >= s ?"bg-primary_blue" :"bg-neutral_300"
-                      }`}
-                    ></div>
-                  )}
-                  {s !== 4 && step === s && (
-                    <>
-                      <div className="w-12 h-2 mb-8 rounded-full bg-primary_blue"></div>
-                      <div className="w-12 h-2 mb-8 rounded-r-full bg-neutral_300"></div>
-                    </>
-                  )}
+              <div className="divide-y flex flex-col">
+                <div className="flex flex-row">
+                  {[1, 2, 3, 4].map((s) => (
+                    <div key={s} className="flex items-center">
+                      <div
+                        className={`w-9 h-9 mx-4 mb-8 flex items-center justify-center ${
+                          step >= s ? "bg-primary_blue" : "bg-neutral_300"
+                        } text-white rounded-full`}
+                      >
+                        {s}
+                      </div>
+                      {s !== 4 && s !== step && (
+                        <div
+                          className={`w-24 h-2 mb-8 rounded-full ${
+                            step >= s ? "bg-primary_blue" : "bg-neutral_300"
+                          }`}
+                        ></div>
+                      )}
+                      {s !== 4 && step === s && (
+                        <>
+                          <div className="w-12 h-2 mb-8 rounded-full bg-primary_blue"></div>
+                          <div className="w-12 h-2 mb-8 rounded-r-full bg-neutral_300"></div>
+                        </>
+                      )}
+                    </div>
+                  ))}
                 </div>
-              ))}
+                <div></div>
+              </div>
             </div>
+
             {step === 1 && (
               <>
                 <div className="pt-16">
-                  <h3 className="flex item-start text-xl font-bold mb-2">
-                    Contact details
-                  </h3>
+                  <h3 className={h3}>Contact details</h3>
                   <p className="flex item-start text-neutral_600 mb-10">
                     Please fill your information so we can get in touch with
                     you.
@@ -124,56 +145,78 @@ const NewPage = () => {
             {step === 2 && (
               <div>
                 <div className="pt-16">
-                  <h3 className="flex item-start text-xl font-bold mb-2">
-                    Our services
-                  </h3>
+                  <h3 className={h3}>Our services</h3>
                   <p className="flex item-start text-neutral_600 mb-10">
                     Please select which service you are interested in.
                   </p>
                 </div>
 
-                <div className="grid grid-cols-2 gap-6">
-                  <div className="rounded-xl shadow-md border border-neutral_300">
-                    <p className="p-11">Development</p>
-                  </div>
-                  <div className="rounded-xl shadow-md border border-neutral_300">
-                    <p className="p-11">Web Design</p>
-                  </div>
-                  <div className="rounded-xl shadow-md border border-neutral_300">
-                    <p className="p-11">Other</p>
-                  </div>
-                  <div className="rounded-xl shadow-md border border-neutral_300">
-                    <p className="p-11">Marketing</p>
-                  </div>
+                <div>
+                  <form className="grid grid-cols-2 gap-6">
+                    {services.map((service) => (
+                      <label>
+                        <input type="checkbox" className="sr-only peer" />
+                        <div className={formButton}>{service.label}</div>
+                      </label>
+                    ))}
+                  </form>
                 </div>
               </div>
             )}
 
-            {step === 3 && <div></div>}
+            {step === 3 && (
+              <div>
+                <div className="pt-16">
+                  <h3 className={h3}>Our services</h3>
+                  <p className="flex item-start text-neutral_600 mb-10">
+                    Please select which service you are interested in.
+                  </p>
+                </div>
+                <div>
+                  <form className="grid grid-cols-2 gap-6">
+                    {services.map((service) => (
+                      <label>
+                        <input type="checkbox" className="sr-only peer" />
 
-            {step === 4 && <div></div>}
-          </div>
-          <div className="flex">
-            {step < 4 && (
-              <div className="flex justify-start mt-6">
+                        <div className={`${formButton} flex items-center`}>
+                          <div className="flex flex-row w-6 h-6 rounded-full border border-neutral_300 peer-checked:bg-primary_blue mr-3"></div>
+                          <p className="">$5.000 - $10.000</p>
+                        </div>
+                      </label>
+                    ))}
+                  </form>
+                </div>
+              </div>
+            )}
+
+            {step === 4 && (
+              <div>
                 <button
-                  className="bg-primary_blue text-white rounded-full text-lg h-[60px] w-[164px]"
-                  onClick={handleNextStep}
+                  className="bg-primary_blue text-white mt-10 rounded-full text-lg h-[60px] w-[164px]"
+                  onClick={handleSubmit}
                 >
-                  Next step
+                  Submit
                 </button>
               </div>
             )}
+          </div>
+          <div className="flex justify-between mt-6">
             {step > 1 && (
-              <div className="flex justify-end mt-6">
-                <button
-                  type="button"
-                  className=" text-primary_blue border border-primary_blue text-white py-3 rounded-full text-lg h-[60px] w-[164px]"
-                  onClick={handlePreviousStep}
-                >
-                  Previous step
-                </button>
-              </div>
+              <button
+                type="button"
+                className="text-primary_blue border border-primary_blue py-3 rounded-full text-lg h-[60px] w-[164px]"
+                onClick={handlePreviousStep}
+              >
+                Previous step
+              </button>
+            )}
+            {step < 4 && (
+              <button
+                className="bg-primary_blue text-white rounded-full text-lg h-[60px] w-[164px]"
+                onClick={handleNextStep}
+              >
+                Next step
+              </button>
             )}
           </div>
         </div>
